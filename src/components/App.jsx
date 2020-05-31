45b0ddb952c7cf0b1cb1bac5d57726e7da3b6191
Xavier Venteo <xavierventeo@gmail.com>
2020-05-31 17:27:27 +0200

    Fixed problems with imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './store/store'
-import Controls from './components/Controls'
+import store from '../store/store'
+import Controls from './Controls'
 
 
 
