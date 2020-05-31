73b4a3d8001e9cb51e1aa241671e7bceb91aab35
Xavier Venteo <xavierventeo@gmail.com>
2020-05-31 18:47:13 +0200

    Call gender API

--- src/actions/genresAction.jsx
+++ src/actions/genresAction.jsx
@@ -2,29 +2,17 @@ import {API_KEY, API_URL} from '../properties'
 import axios from 'axios'
 export const SHOW_GENRES = 'SHOW_GENRES';
 
-
-/*
-export function showGenresAction() {
-    return (dispatch, getState) => {
-        axios.get(`${API_URL}/genre/movie/list?api_key=${API_KEY}&language=es-ES`)
-            .then((response) => {
-                dispatch( { type: SHOW_GENRES, payload: response.data } ) 
-            }) 
-    }   
-}
-*/
-
-
-
 export const showGenresAction = async (dispatch) => {
     try {
         const res = await axios.get(`${API_URL}/genre/movie/list?api_key=${API_KEY}&language=es-ES`);
-        const genres = await res.data.results;
+        console.log(res);
+        const genres = await res.data.genres;
+        console.log(genres);
+
         return dispatch({
             type: SHOW_GENRES,
             payload: genres, 
         });
-
     } catch (error) {
         console.error(error);
     }
