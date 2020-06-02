6aff93aab77f5dfc53da18e4f1afad16cbccda59
Xavier Venteo <xavierventeo@gmail.com>
2020-06-02 09:00:07 +0200

    Add Header to Movie Detail

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-
+import { Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 
 class MovieDetail extends Component {
    
     render() {
         console.log(this.props.match.params.id);
         return (
-        <div>Movie Detail {this.props.match.params.id}</div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/">	← Ir a la página principal</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            </Navbar>       
         );
     }
 }
