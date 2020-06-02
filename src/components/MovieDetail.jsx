b7667a04768ca80fab693e4905bba50275a8bc25
Xavier Venteo <xavierventeo@gmail.com>
2020-06-02 12:23:18 +0200

    Get fields from movie

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { Navbar } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { getMovieDetailAction } from '../actions/moviesAction';
+import { Container, Row, Col, Card} from 'react-bootstrap';
+
 
 class MovieDetail extends Component {
     
@@ -12,10 +14,21 @@ class MovieDetail extends Component {
 
     render() {
         return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/">	← Ir a la página principal  {this.props.movieDetail.title}</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            </Navbar>       
+            <div key="main" id="main">
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="/">	← Ir a la página principal  {this.props.movieDetail.title}</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                </Navbar>     
+                <Card.Img variant="top" src={`http://image.tmdb.org/t/p/w500${this.props.movieDetail.poster_path}`} />
+                <div>Título: {this.props.movieDetail.title}</div>
+                <div>Original Title: {this.props.movieDetail.original_title}</div>
+                <div>Overview: {this.props.movieDetail.overview}</div>
+                <div>Número de votos: {this.props.movieDetail.vote_count}</div>
+                <div>Nota media votos: {this.props.movieDetail.vote_average}</div>
+                <div>Fecha de Lanzamiento: {this.props.movieDetail.release_date}</div>
+                <div>Duración: {this.props.movieDetail.runtime} min</div>
+
+            </div> 
         );
     }
 }
